dd9e03fab4f56685ce7f3d9130699b5f4916b931
Neon Riot <sbrookshire99@gmail.com>
2021-06-14 15:03:38 -0500

    Began work on routes, basic get routes look good

--- server.js
+++ server.js
@@ -18,3 +18,33 @@ app.use(express.static("public"));
 mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/populate", {
   useNewUrlParser: true,
 });
+
+app.get("/", (req, res) => {
+  res.sendFile("index.html");
+});
+
+app.get("/exercise", (req, res) => {
+  res.sendFile("exercise.html", { root: __dirname + "/public" });
+});
+
+app.get("/stats", (req, res) => {
+  res.sendFile("stats.html");
+});
+
+app.get("/api/workouts", (req, res) => {
+  db.Workout.find({})
+    .then((dbWorkout) => {
+      res.json(dbWorkout);
+    })
+    .catch((err) => {
+      res.json(err);
+    });
+});
+
+app.post("/api/workouts", (req, res) => {});
+
+app.put("api/workouts/undefined", (req, res) => {});
+
+app.listen(PORT, () => {
+  console.log(`App running on port ${PORT}!`);
+});
